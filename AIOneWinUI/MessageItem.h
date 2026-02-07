ff44d58ceffda58d0d05989a325bb102e6f75929
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-02-07 21:50:36 +0100

    HMM

--- AIOneWinUI/MessageItem.h
+++ AIOneWinUI/MessageItem.h
@@ -1,36 +1,68 @@
-//#pragma once
-//#include "winrt/Microsoft.UI.Xaml.Data.h"
-//
-//namespace winrt::AIOneWinUI
-//{
-//    struct MessageItem : winrt::implements<MessageItem, winrt::Microsoft::UI::Xaml::Data::INotifyPropertyChanged>
-//    {
-//        MessageItem() = default;
-//
-//        winrt::hstring Role() { return m_role; }
-//        void Role(winrt::hstring const& value)
-//        {
-//            if (m_role != value)
-//            {
-//                m_role = value;
-//                PropertyChanged(*this, winrt::Microsoft::UI::Xaml::Data::PropertyChangedEventArgs{ L"Role" });
-//            }
-//        }
-//
-//        winrt::hstring Text() { return m_text; }
-//        void Text(winrt::hstring const& value)
-//        {
-//            if (m_text != value)
-//            {
-//                m_text = value;
-//                PropertyChanged(*this, winrt::Microsoft::UI::Xaml::Data::PropertyChangedEventArgs{ L"Text" });
-//            }
-//        }
-//
-//        winrt::event<winrt::Microsoft::UI::Xaml::Data::PropertyChangedEventHandler> PropertyChanged;
-//
-//    private:
-//        winrt::hstring m_role;
-//        winrt::hstring m_text;
-//    };
-//}
+#pragma once
+#include "winrt/Microsoft.UI.Xaml.Data.h"
+
+namespace winrt::AIOneWinUI
+{
+    class NotifyPropertyChangedBase {
+    public:
+
+        using PropertyChangedEventHandler = winrt::Microsoft::UI::Xaml::Data::PropertyChangedEventHandler;
+        using PropertyChangedEventArgs = winrt::Microsoft::UI::Xaml::Data::PropertyChangedEventArgs;
+
+        NotifyPropertyChangedBase() = default;
+
+        auto PropertyChanged(PropertyChangedEventHandler const& handler) -> winrt::event_token {
+
+            return propertyChangedEvent.add(handler);
+        }
+
+        auto PropertyChanged(winrt::event_token token) -> void {
+
+            propertyChangedEvent.remove(token);
+        }
+
+    protected:
+
+        auto OnPropertyChanged(this auto& self, PropertyChangedEventArgs const& args) -> void {
+
+            self.propertyChangedEvent(self, args);
+        }
+
+        auto OnPropertyChanged(this auto& self, winrt::hstring const& propertyName) -> void {
+
+            self.propertyChangedEvent(self, PropertyChangedEventArgs{ propertyName });
+        }
+
+    private:
+
+        winrt::event<PropertyChangedEventHandler> propertyChangedEvent;
+    };
+
+    class MessageItem : NotifyPropertyChangedBase {
+
+    };
+
+    //class MessageItem// : winrt::implements<MessageItem, winrt::Microsoft::UI::Xaml::Data::INotifyPropertyChanged>
+    //{
+    ////    MessageItem() = default;
+
+    ////    // Getter
+    ////    winrt::hstring Text() const { return m_text; }
+
+    ////    // Setter
+    ////    void Text(winrt::hstring const& value)
+    ////    {
+    ////        if (value != m_text)
+    ////        {
+    ////            m_text = value;
+    ////            PropertyChanged(*this, winrt::Microsoft::UI::Xaml::Data::PropertyChangedEventArgs{ L"Text" });
+    ////        }
+    ////    }
+
+    ////    // INotifyPropertyChanged event
+    ////    winrt::event<winrt::Microsoft::UI::Xaml::Data::PropertyChangedEventHandler> PropertyChanged;
+
+    ////private:
+    ////    winrt::hstring m_text;
+    //};
+}
