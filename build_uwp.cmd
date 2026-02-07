a947da0a3bf10e1cbfe4f4cdeba9955e3a70e55d
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-02-07 06:30:20 +0100

    user the specific flag

--- build_uwp.cmd
+++ build_uwp.cmd
@@ -4,7 +4,7 @@ setlocal
 set SRC_DIR=%~dp0AIOne
 set BUILD_DIR=%~dp0build\AIOneUWP
 
-cmake -S "%SRC_DIR%" -B "%BUILD_DIR%" -A x64 -DCMAKE_SYSTEM_NAME=WindowsStore
+cmake -S "%SRC_DIR%" -B "%BUILD_DIR%" -G "Visual Studio 18 2026" -A x64 -DAIONE_UWP=true
 
 if errorlevel 1 exit /b 1
 
