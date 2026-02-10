fad0f91d5c010885c78f0b58900381d57af6a373
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-02-11 00:51:54 +0100

    classy

--- UITest/UITest.cpp
+++ UITest/UITest.cpp
@@ -23,7 +23,7 @@ using namespace DirectUI;
 void ThrowIfFailed(HRESULT hr) {
   if (hr == S_OK)
     return;
-  throw hr;
+  throw std::exception();
 }
 
 struct LogListener : public IElementListener {
@@ -81,91 +81,132 @@ struct EventListener : public IElementListener {
 
 long (* RealClassFactoryRegister)(CClassFactory *, IClassInfo*) = 0;
 
+class MainWindow {
+
+	NativeHWNDHost *pHWnd;
+    Element *pWizardMain;
+
+public:
+
+	MainWindow() {
+		ThrowIfFailed(CoInitializeEx(NULL, 0));
+
+		ThrowIfFailed(InitProcessPriv(14, NULL, 0, true));
+		ThrowIfFailed(InitThread(2));
+		ThrowIfFailed(RegisterAllControls());
+
+		
+	}
+
+    ~MainWindow() { UnInitProcessPriv(NULL); }
+
+    ATOM GetElementID(UCString string) { return StrToID(string); }
+    ATOM GetElementByID(UCString string) { return StrToID(string); }
+
+	void Init(HINSTANCE hInstance) {
+      NativeHWNDHost::Create(UCString(L"AIOne"), NULL, NULL, 600, 400, 800, 600,
+                             WS_EX_WINDOWEDGE, WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+                             0, &pHWnd);
+
+      DUIXmlParser *pParser;
+
+      ThrowIfFailed(DUIXmlParser::Create(&pParser, NULL, NULL, NULL, NULL));
+
+      pParser->SetParseErrorCallback(
+          [](UCString err1, UCString err2, int unk, void *ctx) {
+            OutputDebugString(std::format(L"err: {}; {}; {}\n", (LPCWSTR)err1,
+                                          (LPCWSTR)err2, unk)
+                                  .c_str());
+            DebugBreak();
+          },
+          NULL);
+
+      ThrowIfFailed(pParser->SetXMLFromResource(IDR_UIFILE1, hInstance,
+                                            (HINSTANCE)hInstance));
+
+      unsigned long deferKey;
+      HWNDElement *hwnd_element;
+
+      HWNDElement::Create(pHWnd->GetHWND(), true, 0, NULL, &deferKey,
+                          (Element **)&hwnd_element);
+
+
+      ThrowIfFailed(pParser->CreateElement((UCString)L"WizardMain",
+                                           hwnd_element, NULL, NULL,
+                                           (Element **)&pWizardMain));
+
+      pWizardMain->SetVisible(true);
+      pWizardMain->EndDefer(deferKey);
+      pHWnd->Host(pWizardMain);
+
+      pHWnd->ShowWindow(SW_SHOW);
+
+      auto *title_elem =
+          pWizardMain->FindDescendent(StrToID((UCString)L"SXTitle"));
+
+      auto *accept_btn = (Button *)pWizardMain->FindDescendent(
+          StrToID((UCString)L"SXWizardDefaultButton"));
+      auto loadButtonId = StrToID((UCString)L"LoadModelButton");
+      auto *reject_btn = (Button *)pWizardMain->FindDescendent(
+          StrToID((UCString)L"LoadModelButton"));
+      // auto loadButtonId = reject_btn->GetID();
+
+      auto *messageInput = (Edit *)pWizardMain->FindDescendent(
+          StrToID((UCString)L"MessageEditBox"));
+
+      auto *progressSpinner = pWizardMain->FindDescendent(
+          StrToID((UCString)L"SXWizardLoadingProgress"));
+
+      LogListener lis;
+      ThrowIfFailed(pWizardMain->AddListener(&lis));
+
+      int btn_count = 0;
+
+      EventListener click_listener([&](Element *elem, Event *ev) {
+        if (ev->flag != GMF_BUBBLED)
+          return;
+
+        if (ev->type == TouchButton::Click) {
+          auto id = elem->GetID();
+          auto tid = ev->target->GetID();
+          if (loadButtonId == tid)
+
+            return;
+          btn_count++;
+          ThrowIfFailed(title_elem->SetContentString(
+              (UCString)std::format(L"Clicked {} times", btn_count).c_str()));
+          progressSpinner->SetVisible(true);
+        } else if (ev->type == Edit::Enter) {
+          progressSpinner->SetVisible(false);
+          Value *txt;
+          messageInput->GetContentString(&txt);
+          ThrowIfFailed(title_elem->SetContentString(
+              (UCString)std::format(L"Entered: {}", (LPCWSTR)txt->GetString())
+                  .c_str()));
+        }
+      });
+      ThrowIfFailed(pWizardMain->AddListener(&click_listener));
+
+      DumpDuiTree(pWizardMain, 0);
+	}
+
+    void Run() {
+      StartMessagePump();
+    }
+
+	void Send(UCString message) {
+
+	}
+};
+
+
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	
-	ThrowIfFailed(CoInitializeEx(NULL, 0));
+	MainWindow *window = new MainWindow();
 
-	ThrowIfFailed(InitProcessPriv(14, NULL, 0, true));
-    ThrowIfFailed(InitThread(2));
+    window->Init(hInstance);
+    window->Run();
 
-	// uncomment to update class definitions
-	// HookClassFactoryRegister();
-    ThrowIfFailed(RegisterAllControls());
-
-
-	NativeHWNDHost* pwnd;
-
-	NativeHWNDHost::Create((UCString)L"Microsoft DirectUI Test", NULL, NULL,
-		//CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-		600, 400, 800, 600,
-		WS_EX_WINDOWEDGE, WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0,&pwnd);
-
-	DUIXmlParser* pParser;
-
-	ThrowIfFailed(DUIXmlParser::Create(&pParser, NULL, NULL, NULL, NULL));
-
-	pParser->SetParseErrorCallback([](UCString err1, UCString err2, int unk, void*ctx) {
-		OutputDebugString(std::format(L"err: {}; {}; {}\n", (LPCWSTR)err1, (LPCWSTR)err2, unk).c_str());
-		DebugBreak();
-	}, NULL);
-
-	auto hr=pParser->SetXMLFromResource(IDR_UIFILE1, hInstance,(HINSTANCE)hInstance);
-
-	unsigned long defer_key;
-	HWNDElement* hwnd_element;
-
-	HWNDElement::Create(pwnd->GetHWND(),true,0,NULL,&defer_key,(Element**)&hwnd_element);
-
-	Element* pWizardMain;
-	hr = pParser->CreateElement((UCString)L"WizardMain", hwnd_element,NULL,NULL,(Element**)&pWizardMain);
-
-	ThrowIfFailed(hr);
-
-
-	pWizardMain->SetVisible(true);
-	pWizardMain->EndDefer(defer_key);
-	pwnd->Host(pWizardMain);
-
-	pwnd->ShowWindow(SW_SHOW);
-
-	auto *title_elem = pWizardMain->FindDescendent(StrToID((UCString)L"SXTitle"));
-
-	auto *accept_btn = (Button *)pWizardMain->FindDescendent(StrToID((UCString)L"SXWizardDefaultButton"));
-	auto *reject_btn = (Button *)pWizardMain->FindDescendent(StrToID((UCString)L"SXWizardAlternateButton"));
-
-	auto *messageInput = (Edit *)pWizardMain->FindDescendent(StrToID((UCString)L"MessageEditBox"));
-
-	auto *progressSpinner = pWizardMain->FindDescendent(StrToID((UCString)L"SXWizardLoadingProgress"));
-
-	LogListener lis;
-	hr = pWizardMain->AddListener(&lis);
-    ThrowIfFailed(hr);
-
-	int btn_count = 0;
-
-	EventListener click_listener([&](Element*elem, Event*ev) {
-		if (ev->flag != GMF_BUBBLED)
-			return;
-		if (ev->type == TouchButton::Click) {
-			btn_count++;
-            ThrowIfFailed(title_elem->SetContentString((UCString)std::format(L"Clicked {} times", btn_count).c_str()));
-			progressSpinner->SetVisible(true);
-		}
-		if (ev->type == Edit::Enter) {
-			progressSpinner->SetVisible(false);
-			Value *txt;
-			messageInput->GetContentString(&txt);
-			ThrowIfFailed(title_elem->SetContentString((UCString)std::format(L"Entered: {}",(LPCWSTR)txt->GetString()).c_str()));
-		}
-	});
-	hr = pWizardMain->AddListener(&click_listener);
-        ThrowIfFailed(hr);
-
-	DumpDuiTree(pWizardMain, 0);
-
-	StartMessagePump();
-
-	UnInitProcessPriv(NULL);
 	return 0;
 }
