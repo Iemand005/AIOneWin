3e8d0011d55be933a32b7cd010e535498c49da81
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-02-10 11:26:19 +0100

    animation and stuf

--- AIOneWinUI/MainWindow.xaml.cpp
+++ AIOneWinUI/MainWindow.xaml.cpp
@@ -24,16 +24,6 @@ using namespace winrt::Windows::Foundation::Collections;
 
 namespace winrt::AIOneWinUI::implementation
 {
-    int32_t MainWindow::MyProperty()
-    {
-        throw hresult_not_implemented();
-    }
-
-    void MainWindow::MyProperty(int32_t /* value */)
-    {
-        throw hresult_not_implemented();
-    }
-
     IObservableVector<AIOneWinUI::MessageControl> MainWindow::Messages() {
         return m_messages;
     }
