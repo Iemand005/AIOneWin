045835e8c2ebc646713df00f9b548b3f1b8398cd
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-02-12 12:51:59 +0100

    More

--- UITest/AIOneDirectUI.cpp
+++ UITest/AIOneDirectUI.cpp
@@ -199,6 +199,17 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
         ThrowIfFailed(peParent->Add(peTouchEdit2));
         //peTouchEdit2Cleaner.release();
     }
+    {
+        TouchEdit2 *peTouchEdit2;
+        ThrowIfFailed(TouchEdit2::Create(peParent, nullptr, (DirectUI::Element **)&peTouchEdit2));
+        // auto peTouchEdit2Cleaner = wil::scope_exit([&] { peTouchEdit2->Destroy(false); });
+
+        ThrowIfFailed(peTouchEdit2->SetSheet(pvRefDuiSheet->GetStyleSheet()));
+        ThrowIfFailed(peTouchEdit2->SetPromptText(L"Test TouchEdit2 here"));
+
+        ThrowIfFailed(peParent->Add(peTouchEdit2));
+        // peTouchEdit2Cleaner.release();
+    }
 
 
     TouchEdit2 *peTouchEdit2;
